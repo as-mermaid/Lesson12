7022b7ae28ef0b8f51c64a7a4714f122a4e277f5
mermaid <mermaid@ANASTASIASMEA3D>
2024-07-29 18:08:58 +0200

    Добавьте файлы проекта.

---
+++ Lesson12/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12
+{
+    static class Circle
+    {
+        static public double GetLength (double r)
+        {
+            return 2 * Math.PI * r;
+        }
+
+        static public double GetArea(double r)
+        {
+            return Math.PI * r * r;
+        }
+
+        static public bool IsPointInside(double x, double y, double r, double x0, double y0)
+        {
+            double pointDistanse = Math.Sqrt(Math.Pow(x0 - x,2)+Math.Pow(y0 - y,2));
+            return pointDistanse <= r;
+        }
+    }
+}
---
+++ Lesson12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Укажите радиус окружности r=");
+                double r = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Длина окружности {0:F2}, площадь круга {1:F2}", Circle.GetLength(r), Circle.GetArea(r));
+
+                Console.Write("Укажите координаты центра окружности x0=");
+                double x0 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Укажите координаты центра окружности y0=");
+                double y0 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Укажите координаты точки x=");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Укажите координаты точки y=");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                if (Circle.IsPointInside(x, y, r, x0, y0) == true)
+                    Console.WriteLine("Точка внутри окружности");
+                else
+                    Console.WriteLine("Точка вне окружности");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString()); 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
